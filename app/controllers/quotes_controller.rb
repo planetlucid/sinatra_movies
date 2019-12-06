f4ef5a5d7da957bfe3c2aea6566fbf26caf820c5
Ryan Spence <planetlucid@gmail.com>
2019-12-06 11:45:33 -0500

    added editing functionality

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,10 @@
 class QuotesController < ApplicationController
+    
+    get '/quotes' do
+        @quotes = Quote.all
+        erb :index
+    end
+
     get '/quotes/new' do
         erb :new
     end
@@ -6,13 +12,23 @@ class QuotesController < ApplicationController
     post '/quotes' do
         quote = Quote.new(quote_params)
         if quote.save
-            redirect '/'
+            redirect '/quotes'
         else
             @errors = [params.to_s]
             erb :failure
         end
     end 
 
+    get '/quotes/:id/edit' do
+        @quote = Quote.find_by(id: params[:id])
+        if @quote
+            erb :edit
+        else
+            @errors = ["invalid quote id"]
+            erb :failures
+        end
+    end
+    
     private
 
     def quote_params
